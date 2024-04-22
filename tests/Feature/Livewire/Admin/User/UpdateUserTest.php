0525b9121040ebdf457135d1930a517e1f03e141
Raboloko <cesartomas317@gmail.com>
2024-04-22 16:15:40 +0200

    nuevo test UpdateUserTest

--- tests/Feature/Livewire/Admin/User/UpdateUserTest.php
+++ tests/Feature/Livewire/Admin/User/UpdateUserTest.php
@@ -2,21 +2,50 @@
 
 namespace Tests\Feature\Livewire\Admin\User;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Http\Livewire\Admin\User\EditUser;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
+use App\Models\User;
+use Livewire\Livewire;
 
 class UpdateUserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    /** @test */
+    public function it_can_update_a_user()
     {
-        $response = $this->get('/');
+        $adminUser = $this->createAdmin();
+        $studentRole = \Spatie\Permission\Models\Role::create(['name' => 'Alumno']);
 
-        $response->assertStatus(200);aa
+        $user = User::create([
+            'name' => 'Student',
+            'email' => 'student@mail.com',
+            'password' => bcrypt('password'), // Cifra la contraseña utilizando bcrypt
+        ]);
+
+        $user->assignRole($studentRole);
+
+        $this->actingAs($adminUser);
+
+        Livewire::test(EditUser::class)
+            ->call('openEditModal', $user)
+            ->assertSet('editForm.name', $user->name)
+            ->assertSet('editForm.email', $user->email)
+            ->assertSet('editForm.email_confirmation', $user->email)
+            ->assertSet('editForm.password', $user->password)
+            ->assertSet('editForm.role', $user->roles->first()->id);
+
+        // Simular la actualización del usuario
+        $userToUpdate = User::find($user->id);
+        $userToUpdate->name = 'Nuevo Nombre';
+        $userToUpdate->email = 'nuevo@ejemplo.com';
+        $userToUpdate->password = bcrypt('nuevacontraseña');
+        $userToUpdate->save();
+
+        $userToUpdate->refresh();
+
+        $this->assertEquals('Nuevo Nombre', $userToUpdate->name);
+        $this->assertEquals('nuevo@ejemplo.com', $userToUpdate->email);
+        $this->assertTrue(Hash::check('nuevacontraseña', $userToUpdate->password));
     }
 }
+
