4d9b69202aeb6f73d61d58bfa52de6d4594b8e6e
quique <enriquealarconjimenez1@gmail.com>
2024-04-27 19:53:10 +0200

    solucionado error seeders al actualizar laravel

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Video::class, 'creator');
     }
+
+    public function getAuthPasswordName()
+    {
+        return 'password';
+    }
 }
