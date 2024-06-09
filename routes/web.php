39b4c74c85a051f3817b819444278d58f19fca34
quique <enriquealarconjimenez1@gmail.com>
2024-06-09 18:09:31 +0200

    solucionado fallo en rutas

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,13 @@ Route::group(['middleware' => 'admin'], function () {
 Route::group(['middleware' => 'admin_or_teacher'], function () {
     Route::get('visits', ShowVisits::class)->name('visit.index');
     Route::get('thematic-areas', ThematicAreas::class)->name('thematic-areas.index');
-    Route::get('places', ListPlaces::class)->name('places.index');
 });
 
 Route::group(['middleware' => 'admin_or_teacher_or_student'], function () {
     Route::get('points-of-interest', ShowPoint::class)->name('points.index');
     Route::get('videos', ListVideos::class)->name('videos.index');
     Route::get('photographies', Photographies::class)->name('photographies.index');
+    Route::get('places', ListPlaces::class)->name('places.index');
 });
 
 Route::get('admin-emails', EmailManager::class)->name('admin.emails');
